76741fee7c17a25b630fc481aa891272980fa810
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-08-14 23:08:37 +0530

    binary search on answer

---
+++ A_Deadline.cpp
@@ -0,0 +1,40 @@
+/*
+██╗   ██╗██╗   ██╗██╗   ██╗██████╗  █████╗      ██╗
+╚██╗ ██╔╝██║   ██║██║   ██║██╔══██╗██╔══██╗     ██║
+ ╚████╔╝ ██║   ██║██║   ██║██████╔╝███████║     ██║
+  ╚██╔╝  ██║   ██║╚██╗ ██╔╝██╔══██╗██╔══██║██   ██║
+   ██║   ╚██████╔╝ ╚████╔╝ ██║  ██║██║  ██║╚█████╔╝
+   ╚═╝    ╚═════╝   ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,d;
+        cin>>n>>d;
+        int l=0,h=n;
+        bool ok=false;
+        while(l<h){
+            int mid=(l+h)/2;
+            if(mid+((d+mid)/(mid+1))<=n){
+                ok=true;
+                l=mid+1;
+                break;
+            }else{
+                h=mid;
+            }
+        }
+        if(ok){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
