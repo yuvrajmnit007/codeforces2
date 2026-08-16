70053a2d63f0a4f6bf864e4cd550ff9cf9e2d991
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-08-16 23:48:03 +0530

    mathematics number of derangements

--- D_Almost_Identity_Permutations.cpp
+++ D_Almost_Identity_Permutations.cpp
@@ -13,10 +13,21 @@ using namespace std;
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--) {
-        
+    int n,k;
+    cin>>n>>k;
+    int ans=1,temp=1;
+    for(int i=2;i<=k;i++){
+        if(i==2){
+            temp=n*(n-1)/2;
+            ans+=temp;
+        }else if(i==3){
+            temp=temp*(n-2)/3;
+            ans+=2*temp;
+        }else{
+            temp=temp*(n-3)/4;
+            ans+=9*temp;
+        }
     }
+    cout<<ans<<endl;
     return 0;
 }
