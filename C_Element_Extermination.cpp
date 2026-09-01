f1033dcc27353fd99224bb1fad871b8c59202ad3
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-09-01 21:05:03 +0530

    easy greedy solution for 1400 rated problem

---
+++ C_Element_Extermination.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        if(arr[n-1]>arr[0]){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
